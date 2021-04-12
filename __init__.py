16025f7c6d19f88eef05b419a15887600cd22a41
PAUL\Enclume <pauljadoul@gmail.com>
2021-04-12 17:36:01 +0200

    change version

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Paul",
     "description" : "",
     "blender" : (2, 80, 0),
-    "version" : (0, 0, 3),
+    "version" : (0, 0, 4),
     "location" : "View3D",
     "warning" : "",
     "category" : "",
