9415aed212fe70c8100daff4699c3c690dca8707
PAUL\Enclume <pauljadoul@gmail.com>
2021-07-07 16:29:30 +0200

    1.0.1

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Paul",
     "description" : "",
     "blender" : (2, 80, 0),
-    "version" : (0, 0, 7),
+    "version" : (1, 0, 1),
     "location" : "View3D",
     "warning" : "",
     "category" : "",
